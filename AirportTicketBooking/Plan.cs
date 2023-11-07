06aed715e9320c75b4e7d7afc4fbb8e885989293
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-11-07 16:44:53 +0200

    plan-class: created the plan class
    this class is used to keep track of the plan data
    it contains a id, name, and capacity of each class.
    in addition to that there is two constructors one is a default
    constructor, and one is used to initalize the values on creation of the
    plan object.

---
+++ AirportTicketBooking/Plan.cs
@@ -0,0 +1,70 @@
+﻿namespace AirportTicketBooking;
+
+public class Plan
+{
+    private static int _id; 
+    public int Id { get; init; }
+    public string Name { get; set; }
+    private int _economyCapacity;
+    private int _businessCapacity;
+    private int _firstClassCapacity;
+
+    public int EconomyCapacity
+    {
+        get => _economyCapacity;
+        set
+        {
+            if (value > 0)
+            {
+                _economyCapacity = value;
+            }
+            _economyCapacity = 0;
+        }
+    }
+    public int BusinessCapacity
+    {
+        get => _businessCapacity;
+        set
+        {
+            if (value > 0)
+            {
+                _businessCapacity = value;
+            }
+            _businessCapacity = 0;
+        }
+    }
+    public int FirstClassCapacity
+    {
+        get => _firstClassCapacity;
+        set
+        {
+            if (value > 0)
+            {
+                _firstClassCapacity = value;
+            }
+            _firstClassCapacity = 0;
+        } 
+    }
+
+    Plan()
+    {
+        Id = _id;
+        Name = Id.ToString();
+        EconomyCapacity = 0;
+        BusinessCapacity = 0;
+        FirstClassCapacity = 0;
+        _id++;
+    }
+
+    Plan(string name, int economyCapacity, int businessCapacity, int firstClassCapacity)
+    {
+        Id = _id;
+        Name = name;
+        EconomyCapacity = economyCapacity;
+        BusinessCapacity = businessCapacity;
+        FirstClassCapacity = firstClassCapacity;
+        _id++;
+    }
+
+
+}
