1162422156ac4669ae353b6e6327e04a15868cb4
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-11-08 12:00:52 +0200

    Airport Class: created the Airport Class
    this class is used to keep track of the flights in the airport and holds
    some information about the airport id, name, country, address, and list
    of flights.
    
    class Airport created

---
+++ AirportTicketBooking/Airport.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace AirportTicketBooking;
+
+public class Airport
+{
+    private static int _id;
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public Country AirportCountry { get; set; }
+    public string Address { get; set; }
+    public List<Flight> Flights { get; private set; }
+
+    public Airport(string name, Country airportCountry, string address, List<Flight> flights)
+    {
+        Id = _id;
+        Name = name;
+        AirportCountry = airportCountry;
+        Address = address;
+        Flights = flights;
+        _id++;
+    }
+}
