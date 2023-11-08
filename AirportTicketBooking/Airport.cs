102a10c33b119062a408e992d856c985c736528f
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-11-08 13:25:42 +0200

    Class Airport: deleted the Flight List
    there is no need to have a flight list in the airport class.

--- AirportTicketBooking/Airport.cs
+++ AirportTicketBooking/Airport.cs
@@ -9,15 +9,12 @@ public class Airport
     public string Name { get; set; }
     public Country AirportCountry { get; set; }
     public string Address { get; set; }
-    public List<Flight> Flights { get; private set; }
-
-    public Airport(string name, Country airportCountry, string address, List<Flight> flights)
+    public Airport(string name, Country airportCountry, string address)
     {
         Id = _id;
         Name = name;
         AirportCountry = airportCountry;
         Address = address;
-        Flights = flights;
         _id++;
     }
 }
