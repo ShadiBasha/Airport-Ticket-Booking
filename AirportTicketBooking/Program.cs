960e2b2bc172a208f0a1bf65dbb20a3a3ab55016
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-11-09 11:37:26 +0200

    Flight class: added add trip method
    added add trip method that addes a trip to the trips field.

--- AirportTicketBooking/Program.cs
+++ AirportTicketBooking/Program.cs
@@ -6,11 +6,6 @@ class Program
 {
     public static int Main()
     {
-        PlanStorage ps = PlanStorage.GetStorageInstanse();
-        ps.ReadFile();
-        Console.WriteLine(ps.ToString());
-        ps.AddPlan(new PlanDetails("dad", 10, 30));
-        ps.WriteInFile();
         return 0;
     }
 }
