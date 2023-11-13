55bc5913c47bded379c7b3e444a9804a0c2963db
Shadi Basha <shadi.basha@foothillsolutions.com>
2023-11-13 15:02:15 +0200

    User class: added the class
    this class holds the data of a user (name, password) and the bookings
    that he/she has.

---
+++ AirportTicketBooking/User.cs
@@ -0,0 +1,25 @@
+﻿namespace AirportTicketBooking;
+
+public class User
+{
+    public string Name { get; private set; }
+    private int Password { get; set; }
+    private List<FlightDetails> _bookings;
+
+    public User(string name, int password)
+    {
+        Name = name;
+        Password = password;
+        _bookings = new List<FlightDetails>();
+    }
+
+    public void BookAFlight(FlightDetails flightDetails)
+    {
+        _bookings.Add(flightDetails);
+    }
+
+    public List<FlightDetails> GetBookings()
+    {
+        return _bookings;
+    }
+}
